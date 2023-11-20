4e99392c560e49a7f9f28065d6323b513099d9dd
Kalpesh Patil <72344646+kalpesh299@users.noreply.github.com>
2023-11-20 14:38:26 +0530

    heading added

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ function App() {
   return (
    
     <div className="w-screen bg-slate-500">
+      <h1 className='text-center font-bold text-2xl font-serif pt-4 text-white'>Movie Information</h1>
      {data.movies.map((el)=>{
             return(
+
               <Moveicard year={el.release_year} poster={el.img} title={el.title} duration={el.duration_minutes} genre={el.genre} director={el.director} des={el.description}/>
 
             )   
